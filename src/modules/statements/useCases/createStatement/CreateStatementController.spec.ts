dd7f9898e13817369a90635c544a3f108ef81709
Thiago Bignotto <bignotto@gmail.com>
2021-10-10 06:54:48 -0300

    create statement controller test

---
+++ src/modules/statements/useCases/createStatement/CreateStatementController.spec.ts
@@ -0,0 +1,48 @@
+import request from "supertest";
+import { Connection } from "typeorm";
+
+import { app } from "../../../../app";
+import createConnection from "../../../../database";
+import { Statement } from "../../entities/Statement";
+
+let connection: Connection;
+
+describe("Authenticate User Controller", () => {
+  beforeAll(async () => {
+    connection = await createConnection();
+    await connection.runMigrations();
+  });
+
+  afterAll(async () => {
+    await connection.dropDatabase();
+    await connection.close();
+  });
+
+  it("should be able to make a deposit", async () => {
+    await request(app).post("/api/v1/users").send({
+      name: "test dude",
+      email: "test@test.com",
+      password: "12345",
+    });
+
+    const response = await request(app).post("/api/v1/sessions").send({
+      email: "test@test.com",
+      password: "12345",
+    });
+
+    const { token } = response.body;
+
+    const deposit = await request(app)
+      .post("/api/v1/statements/deposit")
+      .set({
+        Authorization: `Bearer ${token}`,
+      })
+      .send({ amount: 1, description: "deposit test" });
+
+    const statement: Statement = deposit.body;
+
+    expect(deposit.status).toBe(201);
+    expect(statement.type).toBe("deposit");
+    expect(statement).toHaveProperty("id");
+  });
+});
